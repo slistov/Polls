1ff4ac6279257efa603930b5216a453111fe1e84
Sergey Listov <slistov@mail.ru>
2021-09-20 16:41:43 +0300

    Перед переходом на Generic

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,16 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
+    # ex: /polls/
     path('', views.index, name='index'),
+    # ex: /polls/5/
+    path('<int:poll_id>/', views.poll, name='poll'),
+    # ex: /polls/1/question/2
+    path('<int:poll_id>/question/<int:question_id>/', views.question, name='question'),
+    # ex: /polls/1/question/2/vote/
+    path('<int:poll_id>/question/<int:question_id>/vote/', views.vote, name='vote'),
+    # ex: /polls/1/question/5/results/
+    path('<int:poll_id>/question/<int:question_id>/results/', views.results, name='results')
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,46 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
-# Create your views here.
-from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
+
+from .models import Poll, Question, Answer
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_poll_list = Poll.objects.order_by('-start_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_poll_list': latest_poll_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def poll(request, poll_id):
+    poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request, 'polls/poll.html', {'poll': poll})
+
+def question(request, poll_id, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/question.html', {'question': question})
+
+def results(request, poll_id, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, poll_id, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.answer_set.get(pk=request.POST['answer'])
+    except (KeyError, Answer.DoesNotExist):
+        # Снова показать форму голосования
+        return render(request, 'polls/question.html', {
+            'question': question,
+            'error_message': "Вы не сделали выбор",
+        })
+    else:
+        # selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.poll_id, question.id,)))
